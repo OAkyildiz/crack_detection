b46eb51022ab00a17128342762bd8ae3c43d33fe
Ozan Akyildiz <oakyildiz@wpi.edu>
2017-04-30 09:42:53 -0400

    lets do this

---
+++ keras_cnn1.py
@@ -0,0 +1,70 @@
+# Simple CNN model for CIFAR-10
+import numpy
+import argparse
+
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+from keras.utils import plot_model
+from keras.utils.vis_utils import model_to_dot
+
+ftype = 'float32'
+#ftype ='float64'
+
+#K.set_image_dim_ordering('th')
+
+K.set_floatx('float32')
+# fix random seed for reproducibility
+seed = 24
+numpy.random.seed(seed)
+
+# load data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+# normalize inputs from 0-255 to 0.0-1.0
+X_train = X_train.astype(ftype)
+X_test = X_test.astype(ftype)
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+X_train.reshape((-1, 64, 64, 3))
+X_test.reshape((-1, 64, 64, 3))
+
+# Create the model
+model = Sequential()
+model.add(Conv2D(100, (3, 3), input_shape=(32, 32, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Conv2D(100, (3, 3), activation='elu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(512, activation='elu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+# Compile model
+epochs = 24
+lrate = 0.01
+decay = lrate/epochs
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+print(model.summary())
+plot_model(model, to_file='model_dnn64_elu.png')
+model.save_weights('cifar10_32_elu')
+json_string = model.to_json()
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=100)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
